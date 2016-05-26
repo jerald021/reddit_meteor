9688d5e0b41f6fa6a3440bfe0461f21cbacc99e0
jerald021 <jerald021@gmail.com>
2016-05-26 20:00:21 +0200

    Ordenar comentarios por fecha o votos terminado

--- client/templates/posts/post_page.js
+++ client/templates/posts/post_page.js
@@ -1,5 +1,40 @@
+Template.postPage.onRendered(function(){
+    Session.set('sort', 'dateNew');
+});
+
 Template.postPage.helpers({
     comments: function() {
-        return Comments.find({postId: this._id});
+        var sort;
+        if(Session.get('sort') == 'dateNew'){
+            sort = Comments.find({postId: this._id}, {sort: {submitted: -1} });
+        }
+        else if (Session.get('sort') == 'dateOld'){
+            sort = Comments.find({postId: this._id}, {sort: {submitted: 1} });
+        }
+        else if (Session.get('sort') == 'votesInc'){
+            sort = Comments.find({postId: this._id}, {sort: {votes: -1} });
+        }
+        else if (Session.get('sort') == 'votesDec'){
+            sort = Comments.find({postId: this._id}, {sort: {votes: 1} });
+        }
+        return sort;
+    }
+});
+Template.postPage.events({
+   'click #sort-date': function(){
+       if(Session.get('sort') == 'dateNew'){
+           Session.set('sort', 'dateOld');
+       }
+       else {
+           Session.set('sort', 'dateNew');
+       }
+   },
+    'click #sort-vote': function(){
+        if(Session.get('sort') == 'votesInc'){
+            Session.set('sort', 'votesDec');
+        }
+        else {
+            Session.set('sort', 'votesInc');
+        }
     }
 });
