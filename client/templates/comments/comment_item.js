5c43d790f16021d801b00fe66a74c2cb5cf459c9
jerald021 <jerald021@gmail.com>
2016-05-26 15:53:15 +0200

    votos comentarios acabados

--- client/templates/comments/comment_item.js
+++ client/templates/comments/comment_item.js
@@ -5,5 +5,20 @@ Template.commentItem.helpers({
     smallAvatar: function () {
         var user = Meteor.users.findOne({_id: this.userId});
         return user;
+    },
+    upvotedClass: function() {
+        var userId = Meteor.userId();
+        if (userId && !_.include(this.upvoters, userId)) {
+            return 'btn-primary upvotable';
+        } else {
+            return 'disabled';
+        }
+    }
+});
+
+Template.commentItem.events({
+    'click .upvotable': function(e) {
+        e.preventDefault();
+        Meteor.call('upvoteComment', this._id);
     }
 });
